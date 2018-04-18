d716d344e67abf8846296fe264500252951f0e15
Bernard Mayer <Marcel_Ducamion@yahoo.fr>
2018-04-18 21:07:16 +0200

    MaJ

--- ColsDataType/$run.cmd
+++ ColsDataType/$run.cmd
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 @cls
 @setlocal enabledelayedexpansion
 @rem 65001 UTF8, 1252 	West European Latin, 850 	Multilingual (Latin I), 437 	United States
 @rem cscript.exe //U
 @rem @CHCP 1252
 
-@set homeDir="C:\Laurence\M1-S2\Datas_IN"
+@set homeDir="C:\Laurence\M1-S2\Datas_IN\Export"
 @rem @set sqlBin="C:\CAT_dskD\myTools\sqlite\sqlite3.exe"
 @rem @set sqlHome="C:\RepoGit\BOxi\FRS_Inventory_DocUpdate"
 @rem @set sqlDB=%sqlHome%\BO_inventories.sqlite
@@ -23,13 +22,13 @@ REM @set datasOut="C:\CAT_dskD\_perso\Laurence\Echantillons\enquete.colsDataType
 @echo Analyse des fichiers CSV Stata
 
 REM set f=C:\Laurence\M1-S2\Datas_IN\indiv_2011q1-extrait
-set f=%homeDir%\indiv_2011q1
+set f=%homeDir%\ColsDatatypeindiv_2011q1
 @set datasIn="%f%.csv"
 @set datasOut="%f%.colsDataType.csv"
 rem %pyBin% %script% %datasIn% %datasOut% 
 
-set f=%homeDir%\indiv_2012q4_234L
-%pyBin% %script% %f%.csv %f%.colsDataType.csv
+@rem set f=%homeDir%\indiv_2012q4_234L
+@rem %pyBin% %script% %f%.csv %f%.colsDataType.csv
 
 set f=%homeDir%\indiv_2011q1
 %pyBin% %script% %f%.csv %f%.colsDataType.csv
@@ -45,8 +44,8 @@ set f=%homeDir%\indiv_2012q2
 %pyBin% %script% %f%.csv %f%.colsDataType.csv
 set f=%homeDir%\indiv_2012q3
 %pyBin% %script% %f%.csv %f%.colsDataType.csv
-@rem set f=%homeDir%\indiv_2012q4
-@rem %pyBin% %script% %f%.csv %f%.colsDataType.csv
+set f=%homeDir%\indiv_2012q4
+%pyBin% %script% %f%.csv %f%.colsDataType.csv
 set f=%homeDir%\indiv_2013q1
 %pyBin% %script% %f%.csv %f%.colsDataType.csv
 set f=%homeDir%\indiv_2013q2
@@ -79,33 +78,3 @@ set f=%homeDir%\indiv_2016q3
 %pyBin% %script% %f%.csv %f%.colsDataType.csv
 set f=%homeDir%\indiv_2016q4
 %pyBin% %script% %f%.csv %f%.colsDataType.csv
-
-=======
-@cls
-@setlocal enabledelayedexpansion
-@rem 65001 UTF8, 1252 	West European Latin, 850 	Multilingual (Latin I), 437 	United States
-@rem cscript.exe //U
-@rem @CHCP 1252
-
-@set homeDir=".\"
-@rem @set sqlBin="C:\CAT_dskD\myTools\sqlite\sqlite3.exe"
-@rem @set sqlHome="C:\RepoGit\BOxi\FRS_Inventory_DocUpdate"
-@rem @set sqlDB=%sqlHome%\BO_inventories.sqlite
-@set pyBin="C:\CAT_dskD\myTools\Python\Python36\python.exe"
-@rem @set dataInHome="D:\BOXI\Liste_Docs_TtesCrs\data"
-@rem @set dataInHome="D:\BOXI\Liste_Docs_TtesCrs\Data-FullClientOnly-ToutesCaisses"
-@rem @set dataInPattern="Liste_Docs_*.csv"
-@set script=C:\RepoGit\Stata2Csv\ColsDataType\colsDataType.py
-
-@REM Compilation des fichiers inventaires des FRS en 1 seul
-@rem @set datasIn="C:\CAT_dskD\_perso\Laurence\Echantillons\enquete.test.csv"
-REM @set datasIn="C:\CAT_dskD\_perso\Laurence\Echantillons\enquete.conforme.csv"
-REM @set datasOut="C:\CAT_dskD\_perso\Laurence\Echantillons\enquete.colsDataType.csv"
-
-REM set f=C:\Laurence\M1-S2\Datas_IN\indiv_2011q1-extrait
-set f=C:\Laurence\M1-S2\Datas_IN\indiv_2011q1
-@set datasIn="%f%.csv"
-@set datasOut="%f%.colsDataType.csv"
-@echo Analyse du fichier CSV Stata
-%pyBin% %script% %datasIn% %datasOut% 
->>>>>>> c90402f3bcadc0b04427f2f64c31947a7ebaf7a7
